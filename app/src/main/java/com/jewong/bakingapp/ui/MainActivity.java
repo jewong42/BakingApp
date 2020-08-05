02be91697778b4ca27e9a6560b9514544137606e
jewong42 <jewong42@outlook.com>
2020-08-04 17:18:44 -0700

    Added toolbar.

--- app/src/main/java/com/jewong/bakingapp/ui/MainActivity.java
+++ app/src/main/java/com/jewong/bakingapp/ui/MainActivity.java
@@ -1,17 +1,31 @@
 package com.jewong.bakingapp.ui;
 
-import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.google.android.material.appbar.AppBarLayout;
 import com.jewong.bakingapp.R;
 
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
+
 public class MainActivity extends FragmentActivity {
+
+    AppBarLayout supportActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        Toolbar toolbasr = findViewById(R.id.toolbar);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        setupWithNavController(toolbasr, navController, appBarConfiguration);
     }
 
 }
